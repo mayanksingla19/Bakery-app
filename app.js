258cb6e6d382e603cbe59c30e3c3a34a22cbe0c1
Mayank Singla <76029062+mayanksingla19@users.noreply.github.com>
2021-04-23 19:23:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 
 
